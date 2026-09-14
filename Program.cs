49046c035465af700ce2be3d7b555d41e4f2de5f
WelintonRuan <welintonruan916@gmail.com>
2026-09-14 15:15:58 -0300

    criando a conexao

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+string connectionString = "Server=localhost;Database=zoo;Uid=root;Pwd=Senac2026;";
+
+using var conexao = new MySqlConnection(connectionString);
+
+try
+{
+    conexao.Open();
+    Console.WriteLine("Conexão com o MySQL realizada com sucesso!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao conectar: {ex.Message}");
+}
